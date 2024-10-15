18179ef6e4960846ae23d285cfb69dbdd65f64bb
Aditya Routh <78529538+adiiiii13@users.noreply.github.com>
2024-10-16 01:27:54 +0530

    Create translate.py

---
+++ translate.py
@@ -0,0 +1,11 @@
+# importing the module
+from englisttohindi.englisttohindi import EngtoHindi
+
+# message to be translated
+message = "Yes, I am geeks"
+
+# creating a EngtoHindi() object
+res = EngtoHindi(message)
+
+# displaying the translation
+print(res.convert)
